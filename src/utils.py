fb6533689295946668a4b9b90f6390cbcacb1e42
Jack-42 <johnjack0987@gmail.com>
2023-03-16 17:55:58 -0600

    Add covid spike protein full nucleotide sequence

--- src/utils.py
+++ src/utils.py
@@ -23,14 +23,36 @@ def load_fasta_seq(fasta_path: str):
     return genome
 
 
-def acid_seq_to_genome(seq: str):
+def gen_fasta_file(fpath: str, seqs: list, labels: list):
+    """
+    Generate a FASTA file with the given params.
+    :param str fpath: path to save fasta file to
+    :param list seqs: sequences to write
+    :param labels: corresponding labels for each sequence
+    """
+    with open(fpath, "w") as ofile:
+        for seq, label in zip(seqs, labels):
+            ofile.write(">" + label + "\n" + seq + "\n")
+    print("Successfully saved given seqs/ids to:", fpath)
+
+
+def acid_seq_to_genome(seq: str, save_path: str = None, label: str = ""):
     """
     Convert a sequence of amino acids to a sequence of nucleotides.
     :param seq: str, sequence of amino acids
+    :param save_path: str (optional), path to save full genome to
+    :param label: str (optional), label for fasta file
     :return: str, converted genome
     """
     codon_df = load_codon_whl("../data/codons.txt")
     map_fn = lambda acid: codon_df[codon_df["Letter"] == acid]["Codon"].iloc[0]
     nt_genome = list(map(map_fn, list(seq)))
     nt_genome = ''.join(nt_genome)
+    if save_path is not None:
+        gen_fasta_file(save_path, [nt_genome], [label])
     return nt_genome
+
+
+if __name__ == "__main__":
+    seq1 = load_fasta_seq("../data/covid_spike_protein.fasta")
+    g = acid_seq_to_genome(seq1, "../data/cov_spike_nt_genome.fasta", "Covid-19 Spike Protein")
