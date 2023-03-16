ccd5c175ee91547627654332dfe78d12c454a4c2
Jack-42 <johnjack0987@gmail.com>
2023-03-16 17:51:58 -0600

    Add function to convert amino acid sequence to nucleotide sequence

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,17 @@
+import pandas as pd
+
 """
 @author Jack Ringer
 Date: 3/15/2023
 Description:
     Various utility functions
 """
+
+
+def load_codon_whl(csv_path: str):
+    df = pd.read_csv(csv_path, sep='\t')
+    df = df.rename({'    AminoAcid': "AminoAcid"}, axis="columns")
+    return df
 
 
 def load_fasta_seq(fasta_path: str):
@@ -13,3 +21,16 @@ def load_fasta_seq(fasta_path: str):
     lines = list(map(lambda x: x.strip(), lines))  # remove \n chars
     genome = ''.join(lines)
     return genome
+
+
+def acid_seq_to_genome(seq: str):
+    """
+    Convert a sequence of amino acids to a sequence of nucleotides.
+    :param seq: str, sequence of amino acids
+    :return: str, converted genome
+    """
+    codon_df = load_codon_whl("../data/codons.txt")
+    map_fn = lambda acid: codon_df[codon_df["Letter"] == acid]["Codon"].iloc[0]
+    nt_genome = list(map(map_fn, list(seq)))
+    nt_genome = ''.join(nt_genome)
+    return nt_genome
