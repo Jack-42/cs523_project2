a2777f66ecd8bd69b9c966950558018081440e90
John Allen Ringer <johnjack0987@gmail.com>
2023-03-19 17:41:17 -0600

    Working on calculating escape values in format for antigenic map

--- src/escape_eval.py
+++ src/escape_eval.py
@@ -1,4 +1,6 @@
 import numpy as np
+import pandas as pd
+
 from binding_calculator import BindingCalculator
 
 """
@@ -12,17 +14,45 @@ Description:
 """
 
 
-def main():
+def get_valid_sites(antibody: str, csv_pth: str):
+    cal = BindingCalculator(csv_or_url=csv_pth, eliciting_virus="SARS-CoV-2",
+                            known_to_neutralize=antibody)
+    return set(cal.sites)
+
+
+def get_ebs(antibody: str, csv_pth: str, sites: list):
+    cal = BindingCalculator(csv_or_url=csv_pth, eliciting_virus="SARS-CoV-2",
+                            known_to_neutralize=antibody)
+    ebs = list(map(lambda site: cal.binding_retained([site]), sites))
+    return ebs
+
+
+def main(save_path: str):
+    assert save_path.endswith(".csv")
+    escape_data_csv = "../data/escape_calculator_data.csv"
     edge_muts = np.load("../results/neutral_net_edgemuts.npy",
                         allow_pickle=True)
-    binding_cal = BindingCalculator()  # use default settings
-    edge_sites = list(map(lambda mut_code: int(mut_code[1:-1]), edge_muts))
-    edge_sites = set(edge_sites) & set(binding_cal.sites)
-    edge_bindings = list(
-        map(lambda site: binding_cal.binding_retained([site]), edge_sites))
-    binding_mp = dict(zip(edge_sites, edge_bindings))
-    print(binding_mp)
+    edge_sites = set(list(map(lambda mut_code: int(mut_code[1:-1]), edge_muts)))
+    # antibodies = ['BA.2.75', 'BQ.1.1', 'BA.2', 'XBB', 'BA.1', 'BA.5', 'D614G']
+    antibodies = ['BA.2.75',
+                  'BQ.1.1']  # = antiserum, sites are various antigens
+    # use only sites with data
+    for ab in antibodies:
+        valid_sites = get_valid_sites(ab, escape_data_csv)
+        edge_sites = edge_sites & valid_sites
+    edge_sites = list(edge_sites)
+    # create table
+    print(edge_sites)
+    table = {}
+    for ab in antibodies:
+        table[ab] = get_ebs(ab, escape_data_csv, edge_sites)
+    print(table)
+    df = pd.DataFrame(table)
+    df['antigen'] = edge_sites
+    df = df.set_index('antigen')
+    print(df)
+    return table
 
 
 if __name__ == "__main__":
-    main()
+    main("../results/escape_distance_table.csv")
