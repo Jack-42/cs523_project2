bb4cf6e0f4ad6fbb56ff5ece6893c8519c89b0f3
James Morris <jmorris7@unm.edu>
2023-04-02 15:00:46 -0600

    reworking neutral network size

--- src/network_size.py
+++ src/network_size.py
@@ -15,11 +15,11 @@ def synonymous_size(r_s, length, subs, k):
     """
 
     sum = 0
-    for step in range(0, k + 1):
-        N_k = math.pow(r_s, step) * (math.pow(subs, step)) * (math.comb(length - step, step))
+    for step in range(1, k + 1):
+        N_k = math.pow(r_s, step) * math.pow(subs,step) * (length - step + 1)
         sum += N_k
 
-    print('%.2E' % Decimal(sum))
+    return sum
 
 
 def kplus1(r_s, length, subs, k, k_max=0):
@@ -38,9 +38,11 @@ def kplus1(r_s, length, subs, k, k_max=0):
 
     if k < 1:
         raise Exception('k must be at least 1')
+    
+    results = []
 
     for k_step in range(k, k + k_max):
-        N_k = math.pow(r_s, k_step - 1) * (math.pow(subs, k_step) * (math.comb(length, k_step)))
+        N_k = math.pow(r_s, k_step) * math.pow(subs,k_step) * (length - k_step + 1)
         N_syn = r_s * N_k
         N_syn_str = str(N_syn)
         if (N_syn >= 10000000):
@@ -49,10 +51,30 @@ def kplus1(r_s, length, subs, k, k_max=0):
         N_nonsyn_str = str(N_nonsyn)
         if (N_nonsyn >= 10000000):
             N_nonsyn_str = '%.2E' % Decimal(N_nonsyn)
-        with open(results_path, 'a') as f:
-            f.write(str(k_step) + ',' + str(N_syn_str) + ',' + N_nonsyn_str)
+        
+        results.append([k_step, N_syn_str, N_nonsyn_str])
+    with open(results_path, 'w') as f:
+        for result in results:
+            f.write(str(result[0]) + ',' + str(result[1]) + ',' + str(result[2]))
             f.write("\n")
 
+def qb4():
+    r_s = .8523
+    length = 200
+    subs = 19
+    k = 4
+
+    syn = synonymous_size(r_s, length, subs, k)
+    print('%.3E' % Decimal(syn))
+    tot = (1/r_s) * syn
+    print('%.3E' % Decimal(tot))
+
+    return syn, tot
+
+def qb5():
+    syn, tot = qb4()
+    diff = tot - syn
+    print('%.3E' % Decimal(diff))
 
 def qb7():
     r_s = .8523
@@ -65,4 +87,7 @@ def qb7():
 
 
 if __name__ == "__main__":
+    #synonymous_size(.8523, 200, 19, 3)
+    #qb4()
+    #qb5()
     qb7()
